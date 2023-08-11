6b342de24166a60efcb43fac2b3efadc7d675c30
nourhan <nourhansaeed89@gmail.com>
2023-08-11 07:46:39 -0700

    printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry  point
+ *
+ * Description: Write a C program that print by using printf  function
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
