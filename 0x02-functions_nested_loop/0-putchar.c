dc88a00de879666c9795dce3d3854c1cedec0c32
nourhan <nourhansaeed89@gmail.com>
2023-08-15 17:00:45 -0700

    print

---
+++ 0x02-functions_nested_loop/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - entry  point
+ *
+ * Description: Write a C program that print by using puts function
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	char x[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 6; i++)
+	{
+		_putchar(x[i]);
+		_putchar('\n');
+	}
+
+	return (0);
