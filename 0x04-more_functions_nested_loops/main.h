ca5ca03545242cc537031940ac31ded9af316771
nourhan <nourhansaeed89@gmail.com>
2023-08-17 13:29:53 -0700

    commit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,18 +4,7 @@
 #include <stdio.h>
 
 int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-int add(int a, int b);
 int _isupper(int c);
+int _isdigit(int c);
 
 #endif
