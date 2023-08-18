3bf283056d8dab0598af345f80722c64843b3601
nourhan <nourhansaeed89@gmail.com>
2023-08-17 18:13:31 -0700

    commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,6 +2,7 @@
 
 /**
 * main - tests function that prints if integer is positive or negative
+*
 * Return: 0
 */
 
@@ -11,5 +12,6 @@ int main(void)
 
 	i = 98 % 2;
 	positive_or_negative(i);
+
 	return (0);
 }
