f8647f6f55230fb483020c7955a3acb29da58b74
nourhan <nourhansaeed89@gmail.com>
2023-08-17 17:36:33 -0700

    commit

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,21 +6,20 @@
  */
 void print_diagonal(int n)
 {
-	int i, space;
+	int x, space;
 
 	if (n <= 0)
-	{
-		_putchar('\n');
-	}
-	while (n > 0)
-	{
-	for (i = 0; i < space; i++)
-		_putchar(' ');
 
-		_putchar('\\');
 		_putchar('\n');
+	else
+	{
+		for (x = 1; x <= n; x++)
+		{
+			for (space = 1 ; space <= x; space++)
 
-	space++;
-	n--;
+			_putchar(' ');
+			_putchar(92); /*is equal to '/' char*/;
+			_putchar('\n');
+		}
 	}
 }
