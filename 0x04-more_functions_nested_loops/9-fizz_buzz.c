c3e31968d2206ec7bc8d7f6b5caffbd5ac8e5179
nourhan <nourhansaeed89@gmail.com>
2023-08-18 18:38:37 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - entry  point
+ *
+ * Description:  Fizz-Buzz
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && !(i % 5 == 0))
+			printf("Fizz");
+		else if ((i % 5 == 0) && !(i % 3 == 0))
+			printf("Buzz");
+		else if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz");
+		else
+			printf("%d", i);
+
+		if (i < 100)
+			printf(" ");
+	}
+
+	printf("\n");
+	return (0);
+}
