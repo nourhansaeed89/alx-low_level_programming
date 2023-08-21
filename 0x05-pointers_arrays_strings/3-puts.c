44040e9317b1bf3a143ea1e2f57acfcbcab99844
nourhan <nourhansaeed89@gmail.com>
2023-08-21 16:53:09 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - function that prints a string, followed by a new line
+ * @str: input string
+ */
+void _puts(char *str)
+{
+	int i
+		for (i = 0; *str != '\0'; str++)
+		{
+			_putchar(*str);
+			i++
+		}
+		 _putchar('\n');
+}
+
+
