abe89987819d6c95562fca764130700aa1c98c43
nourhan <nourhansaeed89@gmail.com>
2023-08-21 16:02:48 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int -function that swaps the values of two integers
+ * @a: pointer1
+ * @b: pointer2
+ */
+void swap_int(int *a, int *b)
+{
+	int storage;
+
+	storage = *a;
+	*a = *b;
+	*b = storage;
