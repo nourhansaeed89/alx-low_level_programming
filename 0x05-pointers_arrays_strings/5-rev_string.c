07af105972bd6976a2656eff6b3186b9d011ce92
nourhan <nourhansaeed89@gmail.com>
2023-08-21 18:39:06 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+/**
+ * rev_string - reverses a string
+ * @s: input string
+ */
+void rev_string(char *s)
+{
+	int l = 0;
+	char storage;
+
+for (l = 0; s[l] != '\0'; ++l)
+	;
+
+	for (int i = 0; i < l / 2; i++)
+	{
+	storage = s[i];
+	s[i] = s[l - 1 - i];
+	s[l - 1 - i] = storage;
+	}
+}
