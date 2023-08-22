5f5ef16aefca6e5f07e83f43a1db52177e8b4515
nourhan <nourhansaeed89@gmail.com>
2023-08-21 19:32:56 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: The input
+ *
+ * Return: The length
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	i++;
+	for (i / 2; s[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
