9f85357c1a64eb613a19dff0e2056ca1334cab6c
nourhan <nourhansaeed89@gmail.com>
2023-08-21 20:08:31 -0700

    commit

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -7,10 +7,12 @@
  */
 void print_array(int *a, int n)
 {
-	for (int i = 0; i < n; i++)
+	int i;
+
+	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
-		if (i < n - 1)
+		if (i != ( n - 1))
 		printf(" %d ", a[i]);
 		else
 		printf("%d", a[i]);
