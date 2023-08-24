377857a0c0599434a6fbfd0df9f1caf17b56813b
nourhan <nourhansaeed89@gmail.com>
2023-08-24 16:25:44 -0700

    commit

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,11 +11,13 @@ void reverse_array(int *a, int n)
 {
 	int i;
 	int storage;
+	int j;
 
 	for (i = 0; i < (n - 1); i++)
 	{
+		for (j = (n - 1); i < j; j--)
 		storage = a[i];
-		a[i] = a[n];
-		a[n] = storage;
+		a[i] = a[j];
+		a[j] = storage;
 	}
 }
