c9e884b624e94191d31e1cb5d2d37f554a466b6b
nourhan <nourhansaeed89@gmail.com>
2023-08-24 19:26:34 -0700

    commit

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -5,13 +5,13 @@
  * string_toupper - changes all lowercase letters of a string to uppercase
  * @str: input string
  *
- * Return: pointer to the resulting string
+ * Return: pointer to the resulting str(str)
  */
 char *string_toupper(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\n')
+	while (str[i] != '\0')
 
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
