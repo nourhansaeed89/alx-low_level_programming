b5ae152380c74d762a63181e11cb943ac20dcf7f
nourhan <nourhansaeed89@gmail.com>
2023-08-24 19:00:34 -0700

    commit

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * leet - encodes a string into 1337
+ * @a: input
+ * Return: encoded string
+ */
+char *leet(char *a)
+{
+	int i, j;
+	char leetMap[] = "aAeEoOtTlL";
+	char leetReplace[] = "4433007711";
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+	for (j = 0; leetMap[j] != '\0'; j++)
+	{
+	if (a[i] == leetMap[j])
+	a[i] = leetReplace[j];
+	}
+	}
+	return (s);
+}
