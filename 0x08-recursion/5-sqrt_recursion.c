a9a080b991990b33c58b0551c77283049db8e5af
nourhan <nourhansaeed89@gmail.com>
2023-08-30 16:47:31 -0700

    commit

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,8 +5,27 @@
  * _sqrt_recursion - function that returns the natural square root of a number
  *
  * @n: input number
+ * s: squar root of num
  *
  * Return: If n does not have a natural square root, the function return -1
  */
+int square(int n, int s);
 int _sqrt_recursion(int n)
 {
+	return (square(n, 1));
+}
+/**
+ * square - square root of a number
+ * @n: input number
+ * @s: square root of num
+ *
+ * Return: If n does not have a natural square root, the function return -1
+ */
+int square(int n, int s);
+if (s * s == n)
+	return (s);
+	else if (s * s < n)
+	return (square(n, s + 1));
+	else
+	return (-1)
+	}
