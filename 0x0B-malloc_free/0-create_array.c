c0b1121df9fdf79c201c814c49330db222558a4c
nourhan <nourhansaeed89@gmail.com>
2023-09-04 14:50:24 -0700

    dynamic memory

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include  "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *create_array - function that creates an array of chars
+ * @size: size of array
+ * @c: char
+ *
+ * Return: Returns a pointer to the array, or NULL if it fails
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *s = malloc(size);
+
+	if (size == 0 || s == 0)
+		return (0);
+	while (size--)
+		*s = c;
+	return (s);
+
+
+}
