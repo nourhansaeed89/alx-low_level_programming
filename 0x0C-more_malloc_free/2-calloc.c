c3a484198d25d3f2cd1dfd8f4e2e3b2f8ebb00e5
nourhan <nourhansaeed89@gmail.com>
2023-09-08 07:39:30 -0700

    dynamic

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * *_memset - used to fill memory
+ * @s: pointer
+ * @b: constant integer
+ * @n: number of byte
+ * Return: s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *ptr = s;
+
+	while (n--)
+		*s++ = b;
+	return (ptr);
+}
+
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @nmemb: an array of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *m;
+
+	if (size == 0 || nmemb == 0)
+		return (NULL);
+	_memset(m, 0, sizeof(int) * nmemb);
+
+	return (m);
+}
