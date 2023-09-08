f416bfdaefc08693cca362e1de892101e58d31b3
nourhan <nourhansaeed89@gmail.com>
2023-09-07 19:37:17 -0700

    dynamic

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,20 +15,25 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *str;
 	unsigned int i, j, L1, L2;
 
+	/* check length */
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+	/* count lenght */
 	for (L1 = 0; s1[L1] != '\0'; L1++)
 		;
 
 	for (L2 = 0; s2[L2] != '\0'; L2++)
 		;
+	/* memory recover */
 	str = malloc(L1 + n + 1);
 	if (str == NULL)
 		return (NULL);
+	/* copy1st string*/
 	for (i = 0; s1[i] != '\0'; i++)
 		str[i] = s1[i];
+	/* copy 2nd string*/
 	for (j = 0; j < n; j++)
 	{
 		str[i] = s2[i];
