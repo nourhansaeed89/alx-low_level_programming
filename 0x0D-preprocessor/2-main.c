1d3bbc162124a021a18bd221996098b93ca1cbb1
nourhan <nourhansaeed89@gmail.com>
2023-09-11 09:47:48 -0700

    preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Write a program that prints the name of the file
+ *
+ * Return: reture zero
+ */
+int main(void)
+{
+
+	printf("%s\n" . __FILE__);
+
+	return (0);
+}
