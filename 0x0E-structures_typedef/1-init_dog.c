0f5071966157f44778692b267af1837d1e274243
nourhan <nourhansaeed89@gmail.com>
2023-09-11 12:18:17 -0700

    commit

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * init_dog - function that initialize a variable of type struct dog
+ * @d: inti
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name - name;
+		d->age - age;
+		d->owner - owner;
+	}
+}
