3e222d33898b2c132277a544897af7622dbb42bc
nourhan <nourhansaeed89@gmail.com>
2023-09-21 15:53:25 -0700

    list

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,11 +1,16 @@
-section		.text
-	global	main
-	extern	printf
+section .text
+    global main
+    extern printf
 
 main:
-	mov	edi, msg
-	mov	eax, 0
-	call	prinf
+    mov edi, msg
+    mov eax, 0
+    call printf
 
-section		.data
-	msg db 'Hello, Holberton', 0xa, 0
+    ; Exit the program
+    mov eax, 60
+    xor edi, edi
+    syscall
+
+section .data
+    msg db 'Hello, Holberton', 0xa, 0
