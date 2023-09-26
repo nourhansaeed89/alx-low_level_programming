2853728026b0928e39eb6d4364e2c141e57921c2
nourhan <nourhansaeed89@gmail.com>
2023-09-26 16:33:48 -0700

    lists

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -32,11 +32,10 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	{
 		current = *head;
 
-	while (current->next != NULL)
-	{
+	while (current->next)
 		current = current->next;
+
 		current->next = new_node;
-	}
 	}
 
 	return (new_node);
