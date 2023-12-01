e9ae70cfe5e05385adf0c51ffa08a3e1c853782e
nourhan <nourhansaeed89@gmail.com>
2023-12-01 12:02:15 -0800

    hash

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,7 +12,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *current;
 
-	if (ht == NULL || key == NULL || *key == '\0')
+	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
+		key == NULL || strlen(key) == 0)
 		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
